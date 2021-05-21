5fca8dfbef52fa6501dd1fa37798268671517a24
Kamenev Nikolay <79969997909@mail.ru>
2021-05-21 14:38:58 +0300

    Create MetaDecorator (react-helmet)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App className="page" />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
